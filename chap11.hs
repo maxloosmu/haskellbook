705f2b106314d34750e85eefa39856e4e11c0622
maxloosmu <maxloo@smu.edu.sg>
2020-10-21 17:24:02 +0800

    updated chap11.hs

--- chap11.hs
+++ chap11.hs
@@ -222,8 +222,55 @@ coolestWord = snd . maximum . wordfreq
 
 
 
+data BinaryTree a =
+  Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+mapTree2 _ Leaf = Leaf
+mapTree2 f (Node left a right) = 
+  Node (mapTree2 f left) (f a) (mapTree2 f right)
+testTree =
+  Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+mapExpected =
+  Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+mapOkay =
+  if mapTree2 (+1) testTree == mapExpected
+  then print "yup okay!"
+  else error "test failed!"
+inorder Leaf = []
+inorder (Node left a right) = (inorder left) ++ a 
+  : inorder right
+foldTree :: (a -> b -> b)
+  -> b
+  -> BinaryTree a
+  -> b
+foldTree f b Leaf = b
+foldTree f b (Node left a right) 
+  = (foldTree f tempb left) where
+    tempb = (f a) tempright
+    tempright = foldTree f b right
+foldTree3 :: (a -> b -> b)
+  -> b
+  -> BinaryTree a
+  -> b
+foldTree3 f ac bt = foldr f ac (inorder bt)
+checkFT = foldTree (:) [] testTree
 
-
-
+-- -- Equational Reasoning:
+-- check = foldTree (:) [] 
+--   (Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf))
+-- check2 = (foldTree (:) (((:) 1) (foldTree (:) [] 
+--   (Node Leaf 4 Leaf))) (Node Leaf 3 Leaf))
+-- check3 = (foldTree (:) (((:) 1) (foldTree (:) (((:) 4) 
+--   (foldTree (:) [] Leaf)) Leaf)) (Node Leaf 3 Leaf))
+-- check4 = (foldTree (:) (((:) 1) (foldTree (:) (((:) 4) 
+--   []) Leaf)) (Node Leaf 3 Leaf))
+-- check5 = (foldTree (:) (((:) 1) (foldTree (:) [4] Leaf)) 
+--   (Node Leaf 3 Leaf))
+-- check6 = (foldTree (:) (((:) 1) [4]) (Node Leaf 3 Leaf))
+-- check7 = foldTree (:) [1,4] (Node Leaf 3 Leaf)
+-- check8 = foldTree (:) (((:) 3) (foldTree (:) [1,4] Leaf)) Leaf
+-- check9 = foldTree (:) (((:) 3) [1,4]) Leaf
+-- = [3,1,4]
 
 
