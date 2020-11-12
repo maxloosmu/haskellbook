9122345c3541a56d7c3bdd3fafab685f2b89691f
maxloosmu <maxloo@smu.edu.sg>
2020-11-12 23:35:49 +0800

    updated mostly projects/chap15/testing/

--- chap15.hs
+++ chap15.hs
@@ -1,5 +1,6 @@
 {-  -}
 import Data.Monoid
+import GHC.Types
 
 {-  
 test :: Integer
@@ -25,7 +26,7 @@ test6 :: Last Integer
 test6 = x `mappend` y
 -}
 
-
+{-  
 data Booly a =
   False'
   | True'
@@ -39,11 +40,85 @@ instance Monoid (Booly a) where
 x :: Booly a
 x = True'
 -- -- default: 
-y :: Booly Any
-test :: Booly Any
+-- y :: Booly Any
+-- test :: Booly Any
 -- y :: Booly a
 -- test :: Booly a
+y :: Booly GHC.Types.Any
+test :: Booly GHC.Types.Any
 y = mempty
 test = mappend x y
+coerce :: a -> Booly a -> Booly a
+coerce _ True' = True'
+coerce _ False' = False'
+a :: Booly Integer
+a = coerce 1 True'
+b :: Booly Integer
+b = coerce 2 False'
+ab :: Booly Integer
+ab = mappend a b
+-}
 
+{-  
+data Optional a =
+  Nada | Only a
+  deriving (Eq, Show)
+instance Semigroup a
+  => Semigroup (Optional a) where
+    (<>) Nada Nada = Nada
+    (<>) Nada (Only y) = Only y
+    (<>) (Only x) Nada = Only x
+    (<>) (Only x) (Only y) = Only (x <> y)
+instance Monoid a
+  => Monoid (Optional a) where
+    mempty = Nada
+    mappend = (<>)
+onlySum :: Optional (Sum Integer)
+onlySum = Only (Sum 1)
+test :: Optional (Sum Integer)
+test = onlySum `mappend` onlySum
+onlyFour :: Optional (Product Integer)
+onlyFour = Only (Product 4)
+test2 :: Optional (Product Integer)
+test2 = onlyFour `mappend` onlyFour
+test3 :: Optional (Sum Integer)
+test3 = onlySum `mappend` Nada
+test4 :: Optional (Product Integer)
+test4 = Nada `mappend` onlyFour
+test5 :: Optional [Integer]
+test5 = Only [1] `mappend` Nada
+-}
+
+{-  
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+madlibbin' :: Exclamation
+  -> Adverb
+  -> Noun
+  -> Adjective
+  -> String
+madlibbin' e adv noun adj =
+  e <> "! he said " <>
+  adv <> " as he jumped into his car " <>
+  noun <> " and drove off with his " <>
+  adj <> " wife."
+madlibbinBetter' :: Exclamation
+  -> Adverb
+  -> Noun
+  -> Adjective
+  -> String
+madlibbinBetter' e adv noun adj = 
+  mconcat [e, "! he said ", 
+  adv, " as he jumped into his car ", 
+  noun, " and drove off with his ", 
+  adj, " wife."]
+test :: String
+test = madlibbin' "hey" "quickly" 
+  "seat" "excited"
+test2 :: String
+test2 = madlibbinBetter' "hey" "quickly" 
+  "seat" "excited"
+-}
 
---
+++ projects/chap15/testing/Testing.hs
@@ -0,0 +1,131 @@
+module Testing where
+
+import Data.Monoid
+import Test.QuickCheck 
+
+monoidAssoc :: (Eq m, Monoid m)
+  => m -> m -> m -> Bool
+monoidAssoc a b c =
+  (a <> (b <> c)) == ((a <> b) <> c)
+
+type S = String
+type B = Bool
+type MA = S -> S -> S -> B
+test :: IO ()
+test = quickCheck (monoidAssoc :: MA)
+test2 :: IO ()
+test2 = verboseCheck (monoidAssoc :: MA)
+
+monoidLeftIdentity :: (Eq m, Monoid m)
+  => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+monoidRightIdentity :: (Eq m, Monoid m)
+  => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+mli :: String -> Bool
+mli = monoidLeftIdentity
+mri :: String -> Bool
+mri = monoidRightIdentity
+test3 :: IO ()
+test3 = quickCheck (mli :: String -> Bool)
+test4 :: IO ()
+test4 = quickCheck (mri :: String -> Bool)
+
+{-  
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+madlibbin' :: Exclamation
+  -> Adverb
+  -> Noun
+  -> Adjective
+  -> String
+madlibbin' e adv noun adj =
+  e <> "! he said " <>
+  adv <> " as he jumped into his car " <>
+  noun <> " and drove off with his " <>
+  adj <> " wife."
+randomChar :: Gen Char
+randomChar = elements ['a'..'z']
+randomString :: Gen String
+randomString = listOf randomChar
+genMad :: Gen String
+genMad = do 
+  e <- randomString
+  adv <- randomString
+  noun <- randomString
+  adj <- randomString
+  return (madlibbin' e adv noun adj)
+test5 :: IO ()
+test5 = sample genMad
+-}
+
+
+data Optional a =
+  Nada | Only a
+  deriving (Eq, Show)
+instance Semigroup a
+  => Semigroup (Optional a) where
+    (<>) Nada Nada = Nada
+    (<>) Nada (Only y) = Only y
+    (<>) (Only x) Nada = Only x
+    (<>) (Only x) (Only y) = Only (x <> y)
+instance Monoid a
+  => Monoid (Optional a) where
+    mempty = Nada
+    mappend = (<>)
+instance Arbitrary a => 
+  Arbitrary (Optional a) where
+    arbitrary = do
+      x <- arbitrary
+      frequency 
+        [(1, return Nada), 
+        (1, return $ Only x)]
+
+newtype First' a =
+  First' { getFirst' :: Optional a }
+  deriving (Eq, Show)
+instance Semigroup (First' a) where
+  (<>) (First' Nada) (First' Nada) = First' Nada
+  (<>) (First' Nada) (First' y) = First' y
+  (<>) (First' x) (First' Nada) = First' x
+  (<>) (First' x) _ = First' x
+instance Monoid (First' a) where
+  mempty = First' Nada
+  mappend = (<>)
+firstMappend :: First' a
+  -> First' a
+  -> First' a
+firstMappend = mappend
+type FirstMappend =
+  First' String
+  -> First' String
+  -> First' String
+  -> Bool
+type FstId =
+  First' String -> Bool
+instance Arbitrary a => 
+  Arbitrary (First' a) where
+    arbitrary = do
+      x <- arbitrary
+      frequency 
+        [(1, return $ First' Nada), 
+        (1, return $ First' x)]
+check :: IO ()
+check = do
+  quickCheck (monoidAssoc :: FirstMappend)
+  quickCheck (monoidLeftIdentity :: FstId)
+  quickCheck (monoidRightIdentity :: FstId)
+onlyOne :: First' Integer
+onlyOne = First' (Only 1)
+onlyTwo :: First' Integer
+onlyTwo = First' (Only 2)
+nada :: First' a
+nada = First' Nada
+test6 :: First' Integer
+test6 = onlyOne `mappend` nada
+test7 :: First' a
+test7 = nada `mappend` nada
+test8 :: First' Integer
+test8 = onlyOne `mappend` onlyTwo
