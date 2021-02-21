d02e9eb272a361a9351943adc88d4f8e4884ef9e
maxloosmu <maxloo@smu.edu.sg>
2021-02-21 16:48:08 +0800

    updated mostly nonhaskellbook/GEB/Chap03.hs, and a bit of projects/chap18/ex184/

--- nonhaskellbook/GEB/Chap03.hs
+++ nonhaskellbook/GEB/Chap03.hs
@@ -1,7 +1,5 @@
 module Chap03 where
 
--- sort and nub do not work with 
--- DND and DF
 import Data.List 
   ( sort, nub )
 
@@ -22,17 +20,24 @@ listAxiom = [axiom x y |
 listRule1 :: [DND Int Int]
 listRule1 = [rule1 x | 
   x <- listAxiom]
--- -- this works:
+-- -- this also works:
 listRule11 :: [DND Int Int]
-listRule11 = map rule1 listRule1
-listRule12 :: [DND Int Int]
-listRule12 = map rule1 listRule11
-listRule13 :: [DND Int Int]
-listRule13 = map rule1 listRule12
+listRule11 = take 1000 $ concat $ 
+  iterate (map rule1) listRule1
 fullList :: [DND Int Int]
-fullList = sort (listAxiom ++ 
-  listRule1 ++ listRule11 ++
-  listRule12 ++ listRule13)
+fullList = (nub . sort) (listAxiom ++ 
+  listRule1 ++ listRule11)
+-- -- this works:
+-- listRule11 :: [DND Int Int]
+-- listRule11 = map rule1 listRule1
+-- listRule12 :: [DND Int Int]
+-- listRule12 = map rule1 listRule11
+-- listRule13 :: [DND Int Int]
+-- listRule13 = map rule1 listRule12
+-- fullList :: [DND Int Int]
+-- fullList = sort (listAxiom ++ 
+--   listRule1 ++ listRule11 ++
+--   listRule12 ++ listRule13)
 -- -- this doesn't work:
 -- listRule11 :: [DND Int Int]
 -- listRule11 
@@ -40,9 +45,6 @@ fullList = sort (listAxiom ++
 --     map rule1 (listRule1 ++ 
 --     listRule11)
 --   | otherwise = listRule11
--- fullList :: [DND Int Int]
--- fullList = sort (listAxiom ++ 
---   listRule1 ++ listRule11)
 
 rule2 :: DND Int Int -> 
   [DF Int Int]
@@ -60,17 +62,32 @@ rule3 (DF z x) = concat (do
   if DND (x+1) z == DND a b
     then return [DF z (x+1)]
     else return [])
-listRule3 :: [DF Int Int]
-listRule3 = concat [if z > x 
-  then rule3 (DF z x) 
-  else [] | (DF z x) <- listRule2]
-  ++ listRule2
-listRule31 :: [DF Int Int]
-listRule31 = (nub . sort . concat) 
-  [if z > x 
-  then rule3 (DF z x) 
-  else [] | (DF z x) <- listRule3]
-  ++ listRule3
+applyRule3 :: [DF Int Int] -> 
+  [DF Int Int]
+applyRule3 previousIteration = 
+  (nub . sort . concat)
+  [ if z > x 
+   then rule3 (DF z x) 
+   else []
+  | (DF z x) <- previousIteration
+  ] ++ previousIteration
+applyRule3List :: [DF Int Int]
+applyRule3List = (nub . sort) $
+  take 1000 $ concat $ 
+  iterate applyRule3 listRule2
+-- -- this works, but too tedious, 
+-- -- have to create multiple functions:
+-- listRule3 :: [DF Int Int]
+-- listRule3 = concat [if z > x 
+--   then rule3 (DF z x) 
+--   else [] | (DF z x) <- listRule2]
+--   ++ listRule2
+-- listRule31 :: [DF Int Int]
+-- listRule31 = (nub . sort . concat) 
+--   [if z > x 
+--   then rule3 (DF z x) 
+--   else [] | (DF z x) <- listRule3]
+--   ++ listRule3
 
 axiom2 :: Prime Int
 axiom2 = Prime 2
@@ -78,7 +95,10 @@ rule4 :: DF Int Int -> [Prime Int]
 rule4 (DF z1 z2) = [Prime z1 | 
   z1 == z2 + 1]
 listRule4 :: [Prime Int]
-listRule4 = concat [rule4 x | 
-  x <- listRule31] ++ [axiom2]
+listRule4 = axiom2 : concat [rule4 x | 
+  x <- applyRule3List]
+
+
+
 
 
