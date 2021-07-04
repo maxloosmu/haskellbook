c458a1c6964363269c2647fce71c0bf69504f365
maxloosmu <maxloo@smu.edu.sg>
2021-07-04 08:07:32 +0800

    modified nonhaskellbook/Martin/project/Test08.hs

--- nonhaskellbook/Martin/project/Test08.hs
+++ nonhaskellbook/Martin/project/Test08.hs
@@ -1,22 +1,30 @@
--- {-# LANGUAGE ExtendedDefaultRules #-}
 import Syntax
 import Error
-data Testing a
-  = A {ace :: a, beta :: Testing a}
-  | B {ace :: a}
-  deriving Show
-test = A (1::Int) test2
-test2 = B 2
 
 startVar = VarDecl () "" OkT
 constrVarDecl :: String -> String -> IO ()
 constrVarDecl x y = do
   buildDecl y
   buildDecl x
+  buildFact1 x
+  buildFact2
+  buildFact3 x y
+  buildRule1 x
+  buildRule2 y
+  buildRule3 x
+  buildRule4 y
 buildDecl :: String -> IO ()
 buildDecl x = printVarDecl2 $ startVar
   {nameOfVarDecl = "decl is" ++ x ++ " : Class",
   tpOfVarDecl = booleanT}
+buildFact1 x = putStrLn $ "fact <" ++ x ++ "ClassIncl>"
+buildFact2 = putStrLn "for c: Class"
+buildFact3 x y = putStrLn $ "is" ++ x ++
+  " c --> is" ++ y ++ " c"
+buildRule1 x = putStrLn $ "rule <" ++ x ++ "Incl>"
+buildRule2 x = putStrLn $ "for x: " ++ x
+buildRule3 x = putStrLn $ "if is" ++ x ++ " x"
+buildRule4 x = putStrLn $ "then is" ++ x ++ " x"
 test3 = constrVarDecl "Car" "Vehicle"
 printVarDecl2 :: Show t => VarDecl t -> IO ()
 printVarDecl2 vd = putStrLn $ nameOfVarDecl vd ++
