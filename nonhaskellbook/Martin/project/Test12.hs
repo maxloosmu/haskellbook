569554b2d94fe64af04c9cee9f611f064fe52b09
maxloosmu <maxloo@smu.edu.sg>
2021-07-07 02:08:37 +0800

    updated nonhaskellbook/Martin/project/

---
+++ nonhaskellbook/Martin/project/Test12.hs
@@ -0,0 +1,18 @@
+import Syntax
+import PrintProg
+
+constrVarDecl :: String -> String -> VarDecl ()
+constrVarDecl varnm classnm =
+  VarDecl () varnm
+  (FunT () (ClassT () (ClsNm classnm)) booleanT)
+test3 = constrVarDecl "isCar" "Vehicle"
+test4 = printVarDecl test3
+constrVarDecl2 varnm classnm1 classnm2 =
+  VarDecl () varnm
+  (FunT () (ClassT () (ClsNm classnm1))
+  (FunT () (ClassT () (ClsNm classnm2)) booleanT))
+test5 = constrVarDecl2 "isTaxi" "isCar" "Vehicle"
+test6 = printVarDecl test5
+
+
+
