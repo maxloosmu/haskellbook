2e278d561d3d52ece5fc639bf434604beaf02bfa
Max Loo <maxloo@smu.edu.sg>
2023-04-29 17:13:13 +0800

    added runGen <$> R.getStdGen <*> pure random5chars >>= putStrLn, but not sure how it works

--- nonhaskellbook/course/test/app/Main.hs
+++ nonhaskellbook/course/test/app/Main.hs
@@ -5,8 +5,16 @@ module Main (main) where
 import Lib
 import qualified System.Random as R
 
+main2 :: IO ()
+main2 = randomWithIO >>= putStrLn
+
+randomWithIO :: IO String
+randomWithIO = do
+  gen <- R.getStdGen  
+  return $ take 5 (R.randomRs ('a','z') gen) 
+
 main :: IO ()
-main = randomWithIO >>= putStrLn
+main = runGen <$> R.getStdGen <*> pure random5chars >>= putStrLn
 
 newtype Gen a = MkGen (R.StdGen -> a)
   deriving (Functor)
@@ -14,7 +22,6 @@ newtype Gen a = MkGen (R.StdGen -> a)
 runGen :: R.StdGen -> Gen a -> a
 runGen prng (MkGen g) = g prng
 
-randomWithIO :: IO String
-randomWithIO = do
-  gen <- R.getStdGen  
-  return $ take 5 (R.randomRs ('a','z') gen) 
+random5chars :: Gen String
+random5chars = MkGen $ take 5 . R.randomRs ('a','z')
+
